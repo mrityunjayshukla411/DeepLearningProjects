41bbd3d078558d0ed638a4bd7eb8342cc18a3fd3
Mrityunjay Shukla <69010998+mrityunjayshukla411@users.noreply.github.com>
2022-08-09 17:02:14 +0530

    Spotify Project added

---
+++ SpotifyReviewClassifier/app.py
@@ -0,0 +1,67 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+# import seaborn as sns 
+import string
+# import matplotlib.pyplot as plt
+# from matplotlib import colors
+import tensorflow as tf
+# import tensorflow_hub as hub
+# import tensorflow_text as text
+# from wordcloud import WordCloud
+
+# # Preprocessing and evaluation
+
+from nltk.corpus import stopwords
+# from nltk.stem.snowball import SnowballStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+# from sklearn.model_selection import train_test_split
+# from sklearn.preprocessing import LabelBinarizer
+# from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+import pickle
+
+with open('tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
+with open('lemmatizer.pickle', 'rb') as handle:
+    lb = pickle.load(handle)
+
+def cleaning(text):
+    #remove punctuations and uppercase
+    clean_text = text.translate(str.maketrans('','',string.punctuation)).lower()
+    
+    #remove stopwords
+    clean_text = [word for word in clean_text.split() if word not in stopwords.words('english')]
+    
+    #lemmatize the word
+    sentence = []
+    for word in clean_text:
+        lemmatizer = WordNetLemmatizer()
+        sentence.append(lemmatizer.lemmatize(word, 'v'))
+
+    return ' '.join(sentence)
+
+model_lstm = tf.keras.models.load_model('./Model_LSTM/')
+
+def lstm_prediction(text):
+    clean_text = cleaning(text)
+    seq = tokenizer.texts_to_sequences([clean_text])
+    padded = pad_sequences(seq)
+
+    pred = model_lstm.predict(padded)
+    # Get the label name back
+    result = lb.inverse_transform(pred)[0]
+    
+    return result
+
+st.title('Spotify Review Sentiment Analysis')
+
+text = st.text_input('Write a review of the spotify app in less than 500 words')
+if text:
+    x = lstm_prediction(text)
+    st.markdown(f'## Your Feelings toward Spotify are:- `{x}`')
+    # st.write('Your Feelings toward Spotify are:- ', x)
+
+
+
